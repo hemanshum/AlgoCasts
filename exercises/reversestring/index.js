7f9447e42d57328967b50b0dd0d4aa3262415d6e
Hemanshu <hemanshu.mahajan@gmail.com>
2020-06-27 11:53:17 +0530

    Reverse String - Solution -2

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,16 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+	let revStr;
+	for (let i of str) {
+		if (!revStr) {
+			revStr = i;
+		} else {
+			revStr = i + revStr;
+		}
+	}
+	return revStr;
+}
 
 module.exports = reverse;
